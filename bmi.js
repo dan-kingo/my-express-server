7ed7b9ed63acf7bed97c4d736e5196b6009790f7
Dan Kingo <danieldejen23@gmail.com>
2024-05-27 13:44:30 +0300

    intro to route handler

--- bmi.js
+++ bmi.js
@@ -4,12 +4,13 @@ import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 
 const app = express();
+const PORT = process.env.PORT || "5000";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.get("/", (req, res) => {
+app.get("^/$|/bmi(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "bmi.html"));
 });
 
@@ -25,6 +26,39 @@ app.post("/", (req, res) => {
   );
 });
 
-app.listen("5000", () => {
+// route handler
+app.get(
+  "/about(.html)?",
+  (req, res, next) => {
+    console.log("next is called!");
+    next();
+  },
+  (req, res) => {
+    res.send("<h2>Here we go ...</h2>");
+  }
+);
+
+// we can use also route chaining
+
+const one = (req, res, next) => {
+  console.log("one!");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two!");
+  next();
+};
+const three = (req, res) => {
+  console.log("three!");
+  res.send("<h3>This is what i do!</h3>");
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
+app.get("/*", (req, res) => {
+  res.status(404).send("<h2>404 page not found!</h2>");
+});
+
+app.listen(PORT, () => {
   console.log("server started at 5000!");
 });
