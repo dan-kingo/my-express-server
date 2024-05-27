b958c759744c5a0106992a0d81d1c7c0a77be4ad
Dan Kingo <danieldejen23@gmail.com>
2024-05-27 15:31:56 +0300

    intro to middleware

--- bmi.js
+++ bmi.js
@@ -2,13 +2,39 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
-
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || "5000";
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// using built in middleware
+app.use(express.static(path.join(__dirname, "/public")));
+
+// using third party middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// using cross orign resource sharing (CORS) third-party middleware
+
+const whitelist = [
+  "https://abrshtsegaye.onrender.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log(`Origin : ${origin}`);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS!"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get("^/$|/bmi(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "bmi.html"));
@@ -22,7 +48,7 @@ app.post("/", (req, res) => {
   result = result.toFixed(2);
 
   res.send(
-    `<h3>the BMI of a person with weight ${weight}kg and height ${height}cm is ${result}</h3>`
+    `<h3>the BMI of a person with weight ${weight}kg and height ${height}m is ${result}</h3>`
   );
 });
 
@@ -55,10 +81,20 @@ const three = (req, res) => {
 
 app.get("/chain(.html)?", [one, two, three]);
 
-app.get("/*", (req, res) => {
-  res.status(404).send("<h2>404 page not found!</h2>");
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.send("<h2>404 page not found!</h2>");
+  } else if (req.accepts("json")) {
+    res.json({ err: "404 Not Found!" });
+  } else {
+    res.type("text").send("404 Not Found!");
+  }
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
 app.listen(PORT, () => {
   console.log("server started at 5000!");
 });
