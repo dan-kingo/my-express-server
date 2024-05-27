ca0e442e833d1993682d04c126591ef0fd1ac710
Dan Kingo <danieldejen23@gmail.com>
2024-05-27 15:53:40 +0300

    fix not allowed by cors error for calculate

--- bmi.js
+++ bmi.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const whitelist = [
   "https://abrshtsegaye.onrender.com",
   "http://127.0.0.1:5500",
-  "http://localhost:3500",
+  "http://localhost:5000",
 ];
 
 const corsOptions = {
